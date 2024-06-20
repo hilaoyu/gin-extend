290be1fce9988f9b61d9de4d2ab897276b635bf5
laoyu <hilaoyu@qq.com>
2024-06-20 18:22:30 +0800

    update gin debug

--- engine/gin.go
+++ engine/gin.go
@@ -8,20 +8,19 @@ type GinEngine struct {
 	*gin.Engine
 }
 
-func NewGinEngine() (e *GinEngine) {
+func NewGinEngine(debug ...bool) (e *GinEngine) {
+	if len(debug) > 0 {
+		if debug[0] {
+			gin.SetMode(gin.DebugMode)
+		} else {
+			gin.SetMode(gin.ReleaseMode)
+		}
+	}
 	e = &GinEngine{gin.New()}
 	return
 }
 
 func (e *GinEngine) UseDefault() *GinEngine {
 	e.Use(gin.Logger(), gin.Recovery())
-	return e
-}
-func (e *GinEngine) Debug(debug bool) *GinEngine {
-	if debug {
-		gin.SetMode(gin.DebugMode)
-	} else {
-		gin.SetMode(gin.ReleaseMode)
-	}
 	return e
 }
